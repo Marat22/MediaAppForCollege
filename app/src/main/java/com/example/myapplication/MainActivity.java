d7f082b4f6fbbf4a01e113681d2af85d9ef4b51d
Marat (home comp) <maratsaifutdinov22@gmail.com>
2025-01-15 15:33:58 +0500

    VideoPlayer

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        videoPlayer = findViewById(R.id.videoPlayer);
+        Uri myVideoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoPlayer.setVideoURI(myVideoUri);
+
+        ViewGroup.LayoutParams defaultParams = videoPlayer.getLayoutParams();
+
+        videoPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewGroup.LayoutParams currentParams = videoPlayer.getLayoutParams();
+
+                if (currentParams.width != defaultParams.width && currentParams.height != defaultParams.height) {
+                    videoPlayer.setLayoutParams(defaultParams);
+                } else {
+                    videoPlayer.setLayoutParams(
+                            new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+
+                                    ViewGroup.LayoutParams.MATCH_PARENT));
+                }
+            }
+
+        });
+        videoPlayer.start();
+    }
+
+    public void play(View view) {
+//        R.id.pause
+        videoPlayer.start();
+    }
+
+    public void pause(View view) {
+        videoPlayer.pause();
+    }
+
+    public void stop(View view) {
+        videoPlayer.stopPlayback();
+        videoPlayer.resume();
+    }
+}
