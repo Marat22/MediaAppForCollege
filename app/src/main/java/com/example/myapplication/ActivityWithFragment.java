8d1b0a1ada92efedc9afa6b263e8d79ccfe4a044
Marat (home comp) <maratsaifutdinov22@gmail.com>
2025-01-16 23:34:15 +0500

    Specify date and time

--- app/src/main/java/com/example/myapplication/ActivityWithFragment.java
+++ app/src/main/java/com/example/myapplication/ActivityWithFragment.java
@@ -1,56 +1,71 @@
 package com.example.myapplication;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class ActivityWithFragment extends AppCompatActivity {
 
+    TextView currentDateTime;
+    Calendar dateAndTime=Calendar.getInstance();
     @Override
     public void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_with_fragment);
+        currentDateTime = findViewById(R.id.currentDateTime);
+        setInitialDateTime();
     }
 
-    public void register(View view) {
-        String name = ((EditText) findViewById(R.id.name)).getText().toString();
-        String email = ((EditText) findViewById(R.id.email)).getText().toString();
-        String password = ((EditText) findViewById(R.id.password)).getText().toString();
-
-        if (name.isEmpty()) {
-            CustomDialogFragment dialog = new CustomDialogFragment("Name field is not specified!");
-            dialog.show(getSupportFragmentManager(), "custom");
-            return;
-        }
-
-        if (email.isEmpty()) {
-            CustomDialogFragment dialog = new CustomDialogFragment("E-mail field is not specified!");
-            dialog.show(getSupportFragmentManager(), "custom");
-            return;
-        }
-
-        if (password.isEmpty()) {
-            CustomDialogFragment dialog = new CustomDialogFragment("Password field is not specified!");
-            dialog.show(getSupportFragmentManager(), "custom");
-            return;
-        }
-
-        registerNewUser(name, email, password);
+    // отображаем диалоговое окно для выбора даты
+    public void setDate(View v) {
+        new DatePickerDialog(ActivityWithFragment.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
     }
 
-
-    private void registerNewUser( String name, String email, String password) {
-        return;
+    // отображаем диалоговое окно для выбора времени
+    public void setTime(View v) {
+        new TimePickerDialog(ActivityWithFragment.this, t,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
+                .show();
     }
-//    public void showDialog(View v) {
-//
-//    }
+    // установка начальных даты и времени
+    private void setInitialDateTime() {
 
+        currentDateTime.setText(DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME));
+    }
 
+    // установка обработчика выбора времени
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+            setInitialDateTime();
+        }
+    };
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
 }
